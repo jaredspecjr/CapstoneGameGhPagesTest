4c454d8a4aa6b58d88c5ee356550765f3d4ce1a1
jared <jaredspecjr@gmail.com>
2018-07-20 14:52:15 -0700

    add template for new project with jsx files

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Room1 from './Room1';
+import { Route, Switch, HashRouter } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <div className="stage">
+            <Switch>
+              <Route exact path='/' component={Room1} />
+            </Switch>
+          </div>
+          <div className="controls">
+            <p>controls will go here</p>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
