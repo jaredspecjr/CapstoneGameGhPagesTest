ee970c2a683630d5df62b64c82e4536df2bbce0b
jared <jaredspecjr@gmail.com>
2018-07-20 16:13:54 -0700

    add image to room1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Room1 from './Room1';
-// import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -8,15 +8,35 @@ class App extends Component {
 
       <div className="App">
         <div className="stage">
-
-          <p>this works</p>
+          <Switch>
+            <Route exact path="/" render={()=> <Room1/>} />
+          </Switch>
         </div>
         <div className="controls">
           <p>controls will go here</p>
         </div>
         <style jsx>{`
-          .stage {
+          .App {
             text-align: center;
+          }
+          .stage {
+            border: 1px solid black;
+            margin-top: 30px;
+            margin-right: 250px;
+            margin-left: 250px;
+            background-color: black;
+            min-width: 400px;
+
+          }
+          .controls {
+            border: 10px solid black;
+            margin-top: 100px;
+            height: 279px;
+          }
+          img {
+            width: 100%;
+            min-width: 400px;
+
           }
       `}</style>
       </div>
---
+++ src/components/Room1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import room1 from "../assets/images/room1.jpg";
+
+function Room1(){
+  return (
+    <div>
+      <div>
+        <img src={room1}/>
+      </div>
+    </div>
+  )
+}
+
+export default Room1;
