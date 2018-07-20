9d141c95c25907850b93bbe80b40a797492d2ccb
jared <jaredspecjr@gmail.com>
2018-07-20 15:14:06 -0700

    fix issues with installing url and file loaders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import Room1 from './Room1';
-import { Route, Switch, HashRouter } from 'react-router-dom';
+// import { Route, Switch, HashRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="App">
-          <div className="stage">
-            <Switch>
-              <Route exact path='/' component={Room1} />
-            </Switch>
-          </div>
-          <div className="controls">
-            <p>controls will go here</p>
-          </div>
+
+      <div className="App">
+        <div className="stage">
+
+          <p>this works</p>
         </div>
-      </HashRouter>
+        <div className="controls">
+          <p>controls will go here</p>
+        </div>
+      </div>
+
     );
   }
 }
